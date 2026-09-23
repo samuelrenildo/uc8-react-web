03ef8c1bd78a1bf3bf502d264635403fbdfc35c9
SAMUEL RENILDO DE ALMEIDA NOGUEIRA <a95984530@rn.senac.local>
2026-09-23 20:32:47 -0300

    fix: usa dados de teste próprios em vez de exemplo do enunciado

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import type { Livro } from './types/entidades';
 import { CartaoLivro } from './componentes/CartaoLivro';
 
 
-const domCasmurro: Livro = {
+const Livro1 : Livro = {
   id: 1,
-  titulo: 'Dom Casmurro',
-  autor: 'Machado de Assis',
-  sinopse: 'Romance narrado por Bentinho.',
+  titulo: 'Pai Rico, Pai Pobre',
+  autor: 'Robert Kiyosaki',
+  sinopse: 'é um guia clássico de finanças pessoais que desafia o modelo tradicional de buscar um emprego seguro para acumular riqueza.',
   exemplares: 3,
 };
 
-const vidasSecas: Livro = {
+const Livro2: Livro = {
   id: 2,
-  titulo: 'Vidas Secas',
-  autor: 'Graciliano Ramos',
+  titulo: '1984',
+  autor: 'George Orwell',
   exemplares: 1,
 };
 
@@ -21,8 +21,8 @@ const vidasSecas: Livro = {
 export default function App() {
   return (
     <main>
-      <CartaoLivro livro={domCasmurro} />
-      <CartaoLivro livro={vidasSecas} minimoExemplares={3} />
+      <CartaoLivro livro={Livro1} />
+      <CartaoLivro livro={Livro2} minimoExemplares={3} />
     </main>
   );
 }
